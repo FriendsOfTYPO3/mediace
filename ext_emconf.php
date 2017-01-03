d05b12cea4a3e33cf84b0aae2446337a9c064ecb
Ephraim Härer <ephraim.haerer@renolit.com>
2017-01-03 13:29:52 +0100

    fixed problems with flexform wizard and raised version to 7.6.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,20 +1,40 @@
 <?php
-$EM_CONF[$_EXTKEY] = array(
-    'title' => 'Media Content Element',
-    'description' => 'The media functionality from TYPO3 6.2 and earlier can be found here. This extension provides ContentObjects and Content Elements.',
-    'category' => 'fe',
-    'author' => 'Friends of TYPO3',
-    'author_email' => 'friendsof@typo3.org',
-    'state' => 'stable',
-    'uploadfolder' => 0,
-    'createDirs' => 'uploads/media',
-    'clearCacheOnLoad' => 1,
-    'version' => '7.6.3',
-    'constraints' => array(
-        'depends' => array(
-            'typo3' => '7.6.0-7.6.99',
-        ),
-        'conflicts' => array(),
-        'suggests' => array(),
-    )
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "mediace".
+ *
+ * Auto generated 07-06-2016 09:51
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'Media Content Element',
+  'description' => 'The media functionality from TYPO3 6.2 and earlier can be found here. This extension provides ContentObjects and Content Elements.',
+  'category' => 'fe',
+  'author' => 'Friends of TYPO3',
+  'author_email' => 'friendsof@typo3.org',
+  'state' => 'stable',
+  'uploadfolder' => false,
+  'createDirs' => 'uploads/media',
+  'clearCacheOnLoad' => 1,
+  'version' => '7.6.4',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '7.6.0-7.6.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+  'clearcacheonload' => true,
+  'author_company' => NULL,
 );
+
